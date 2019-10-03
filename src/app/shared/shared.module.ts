8746075407bba422a726e9ea81c6892a1d8d2bb4
paulkoustav372 <paulkoustav372@gmail.com>
2019-10-03 14:53:03 +0530

    upload service added in shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
+
+import { UploadService } from "./upload.service";
+
 import { AddupdatecartbtnComponent } from './addupdatecartbtn/addupdatecartbtn.component';
 import { FileuploadComponent } from './fileupload/fileupload.component';
 
@@ -12,6 +15,7 @@ import { FileuploadComponent } from './fileupload/fileupload.component';
     CommonModule,
     SharedRoutingModule
   ],
+  providers: [UploadService],
   exports: [AddupdatecartbtnComponent,FileuploadComponent]
 })
 export class SharedModule { }

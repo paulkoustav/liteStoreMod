da941c96ae981eca689bfee2a17c13bf57f90392
paulkoustav372 <paulkoustav372@gmail.com>
2019-10-03 13:04:24 +0530

    common data service and userdata service added

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -5,12 +5,15 @@ import { UserRoutingModule } from './user-routing.module';
 import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
 
+import { UserdataService } from './userdata.service';
+
 
 @NgModule({
   declarations: [RegistrationComponent, LoginComponent],
   imports: [
     CommonModule,
     UserRoutingModule
-  ]
+  ],
+  providers: [UserdataService]
 })
 export class UserModule { }

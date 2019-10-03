be4fa6df7e0cbdcfa957a0f27c3eb52c72632d3c
paulkoustav372 <paulkoustav372@gmail.com>
2019-10-03 15:57:01 +0530

    auth service added in user module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -5,6 +5,7 @@ import { UserRoutingModule } from './user-routing.module';
 
 import { SharedModule } from '../shared/shared.module';
 import { UserdataService } from './userdata.service';
+import {AuthService} from './auth.service';
 
 import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
@@ -16,6 +17,6 @@ import { LoginComponent } from './login/login.component';
     UserRoutingModule,
     SharedModule
   ],
-  providers: [UserdataService]
+  providers: [UserdataService,AuthService]
 })
 export class UserModule { }
